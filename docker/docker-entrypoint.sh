3c25b86ce03962edaeb18c599b4f1c4f127ea63d
Tobias Glatthar <t0bst4r@outlook.com>
2024-06-13 17:18:46 +0200

    fix(docker): fixed config file condition

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd /app || exit 1
 
-if [ -z "$CONFIG_FILE" ]; then
+if [ -n "$CONFIG_FILE" ]; then
   HOME_ASSISTANT_URL=$(jq -r --arg defValue "$HOME_ASSISTANT_URL" '.homeAssistantUrl // $defValue' "$CONFIG_FILE")
   HOME_ASSISTANT_ACCESS_TOKEN=$(jq -r --arg defValue "$HOME_ASSISTANT_ACCESS_TOKEN" '.homeAssistantAccessToken // $defValue' "$CONFIG_FILE")
   HOME_ASSISTANT_CLIENT_CONFIG=${HOME_ASSISTANT_CLIENT_CONFIG:-"{}"}
