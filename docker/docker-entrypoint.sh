8919171ad58408d54493fa0766adf1ce98432fbd
t0bst4r <t0bst4r@outlook.com>
2024-06-13 12:38:20 +0200

    feat(docker): #44 add config-file support for docker deployments

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 
 cd /app || exit 1
+
+if [ -z "$CONFIG_FILE" ]; then
+  HOME_ASSISTANT_URL=$(jq -r --arg defValue "$HOME_ASSISTANT_URL" '.homeAssistantUrl // $defValue' "$CONFIG_FILE")
+  HOME_ASSISTANT_ACCESS_TOKEN=$(jq -r --arg defValue "$HOME_ASSISTANT_ACCESS_TOKEN" '.homeAssistantAccessToken // $defValue' "$CONFIG_FILE")
+  HOME_ASSISTANT_CLIENT_CONFIG=${HOME_ASSISTANT_CLIENT_CONFIG:-"{}"}
+  HOME_ASSISTANT_CLIENT_CONFIG=$(jq -r --argjson defValue "$HOME_ASSISTANT_CLIENT_CONFIG" '.homeAssistantClientConfig // $defValue' "$CONFIG_FILE")
+  export HOME_ASSISTANT_URL HOME_ASSISTANT_ACCESS_TOKEN HOME_ASSISTANT_CLIENT_CONFIG
+fi
+
 npx matterbridge -add ./node_modules/matterbridge-home-assistant
 npx matterbridge -bridge
