b84adb95502dc8e651ce4487f4f6db6d66149bfa
t0bst4r <t0bst4r@outlook.com>
2024-06-18 22:56:32 +0200

    fix(ci): updated readme during release

--- .release-it.cjs
+++ .release-it.cjs
@@ -16,6 +16,13 @@ module.exports = {
         name: 'angular',
       },
     },
+    '@j-ulrich/release-it-regex-bumper': {
+      out: {
+        file: 'README.md',
+        search: 'matterbridge-home-assistant&v=([0-9.]+)',
+        replace: 'matterbridge-home-assistant&v={{version}}',
+      },
+    },
   },
   hooks: {
     'after:release': ['yarn pack --filename matterbridge-home-assistant.tgz'],
