3175127da204ec4fd04830becbf6ac0c7c9cd28a
t0bst4r <t0bst4r@outlook.com>
2024-07-24 19:16:42 +0200

    fix: remove failure state from docker-entrypoint

--- packages/matterbridge-home-assistant-docker/docker-entrypoint.sh
+++ packages/matterbridge-home-assistant-docker/docker-entrypoint.sh
@@ -2,14 +2,6 @@
 
 cd /app || exit 1
 
-if [ ! -f "$MHA_CONFIG_FILE" ]; then
-  echo "Matterbridge-Home-Assistant has to be configured by a config file."
-  echo "You can configure the config-file location by setting the MHA_CONFIG_FILE environment variable."
-  echo "Please see the documentation for further details about the structure of the config file: "
-  echo "https://github.com/t0bst4r/matterbridge-home-assistant/tree/-/packages/matterbridge-home-assistant#configuration"
-  exit 2
-fi
-
 matterbridge -add matterbridge-home-assistant
 
 MATTERBRIDGE_OPTIONS=("-bridge" "-docker")
