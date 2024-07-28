2fd1b5a3920703459575e31d040c55bc3b8e3a46
Tobias Glatthar <t0bst4r@outlook.com>
2024-07-28 21:56:54 +0200

    feat(docker): allow specifiying matter and frontend ports (#229)

--- packages/matterbridge-home-assistant-docker/docker-entrypoint.sh
+++ packages/matterbridge-home-assistant-docker/docker-entrypoint.sh
@@ -4,6 +4,6 @@ cd /app || exit 1
 
 matterbridge -add matterbridge-home-assistant
 
-MATTERBRIDGE_OPTIONS=("-bridge" "-docker" "-logger $LOG_LEVEL")
+MATTERBRIDGE_OPTIONS=("-bridge" "-docker" "-logger $LOG_LEVEL" "-frontend $FRONTEND_PORT" "-port $MATTER_PORT")
 
 matterbridge "${MATTERBRIDGE_OPTIONS[@]}"
