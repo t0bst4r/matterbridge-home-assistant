3434f074e9d92ba713d73d9db4a6cb9b5edf4ee6
Tobias Glatthar <t0bst4r@outlook.com>
2024-07-29 07:15:08 +0200

    fix(cover): add identify aspect

--- packages/home-assistant-matter-hub/src/devices/cover-device.ts
+++ packages/home-assistant-matter-hub/src/devices/cover-device.ts
@@ -1,6 +1,6 @@
 import { DeviceTypes } from 'matterbridge';
 
-import { WindowCoveringAspect } from '@/aspects/index.js';
+import { IdentifyAspect, WindowCoveringAspect } from '@/aspects/index.js';
 import { HomeAssistantClient } from '@/home-assistant-client/index.js';
 import { HomeAssistantMatterEntity } from '@/models/index.js';
 
@@ -10,6 +10,7 @@ export class CoverDevice extends DeviceBase {
   constructor(client: HomeAssistantClient, entity: HomeAssistantMatterEntity, config: DeviceBaseConfig) {
     super(entity, DeviceTypes.WINDOW_COVERING, config);
 
+    this.addAspect(new IdentifyAspect(this.matter, entity));
     this.addAspect(new WindowCoveringAspect(client, entity, this.matter));
   }
 }
