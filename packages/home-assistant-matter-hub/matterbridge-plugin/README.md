1f45d05fb1036979be48aa9816019f847ce8e0dd
t0bst4r <t0bst4r@outlook.com>
2024-08-05 20:43:54 +0200

    chore: extracted matterbridge plugin into home-assistant-matter-hub scope

---
+++ packages/home-assistant-matter-hub/matterbridge-plugin/README.md
@@ -0,0 +1,4 @@
+# @home-assistant-matter-hub/matterbridge-plugin
+
+This package implements a plugin for [matterbridge](https://github.com/Luligu/matterbridge/) to provide Home Assistant
+Entities as Matter devices.
--- packages/matterbridge-home-assistant/src/index.ts
+++ packages/matterbridge-home-assistant/src/index.ts
@@ -1,19 +1,3 @@
-import { logger } from '@home-assistant-matter-hub/core';
-import type { Matterbridge, PlatformConfig } from 'matterbridge';
-import type { AnsiLogger } from 'matterbridge/logger';
-import * as ws from 'ws';
+import initializePlugin from '@home-assistant-matter-hub/matterbridge-plugin';
 
-import { NodeAnsiTransport } from './node-ansi-transport.js';
-import { HomeAssistantPlatform } from './platform.js';
-
-const wnd = globalThis as Record<string, unknown>;
-wnd.WebSocket = ws.WebSocket;
-
-export default function initializePlugin(
-  matterbridge: Matterbridge,
-  ansiLogger: AnsiLogger,
-  platformConfig: PlatformConfig,
-): HomeAssistantPlatform {
-  logger.add(new NodeAnsiTransport(ansiLogger));
-  return new HomeAssistantPlatform(matterbridge, ansiLogger, platformConfig);
-}
+export default initializePlugin;
