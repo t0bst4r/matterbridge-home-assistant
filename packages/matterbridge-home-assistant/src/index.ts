48a0bbed8812616593ef1a884e6c65b29a330c1b
t0bst4r <t0bst4r@outlook.com>
2024-08-14 08:22:14 +0200

    fix(matterbridge): patch NodeAnsiLogger until fix is provided by matterbridge (#262)

--- packages/matterbridge-home-assistant/src/index.ts
+++ packages/matterbridge-home-assistant/src/index.ts
@@ -1,6 +1,6 @@
 import { logger } from '@home-assistant-matter-hub/core';
 import type { Matterbridge, PlatformConfig } from 'matterbridge';
-import type { AnsiLogger } from 'matterbridge/logger';
+import { AnsiLogger, TimestampFormat } from 'matterbridge/logger';
 import * as ws from 'ws';
 
 import { NodeAnsiTransport } from './node-ansi-transport.js';
@@ -8,6 +8,18 @@ import { HomeAssistantPlatform } from './platform.js';
 
 const wnd = globalThis as Record<string, unknown>;
 wnd.WebSocket = ws.WebSocket;
+
+if (!Object.prototype.hasOwnProperty.call(AnsiLogger.prototype, 'setGlobalCallback')) {
+  Object.assign(AnsiLogger.prototype, {
+    setGlobalCallback: AnsiLogger.setGlobalCallback.bind(AnsiLogger),
+    getGlobalCallback: AnsiLogger.getGlobalCallback.bind(AnsiLogger),
+  });
+  const patchLogger = new AnsiLogger({
+    logTimestampFormat: TimestampFormat.TIME_MILLIS,
+    logName: 'matterbridge-home-assistant',
+  });
+  patchLogger.warn('Patched NodeAnsiLogger until this PR is released: https://github.com/Luligu/matterbridge/pull/98');
+}
 
 export default function initializePlugin(
   matterbridge: Matterbridge,
