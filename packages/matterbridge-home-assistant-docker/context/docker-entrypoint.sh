dc2b5e40d38642eae3054e6391764fa829721eaf
김도훈 <dohun0310@naver.com>
2024-08-17 14:52:54 +0900

    chore: expose SSL port in Dockerfile and enable SSL in matterbridge o… (#265)

--- packages/matterbridge-home-assistant-docker/context/docker-entrypoint.sh
+++ packages/matterbridge-home-assistant-docker/context/docker-entrypoint.sh
@@ -6,4 +6,8 @@ matterbridge -add matterbridge-home-assistant
 
 MATTERBRIDGE_OPTIONS=("-bridge" "-docker" "-logger $LOG_LEVEL" "-frontend $FRONTEND_PORT" "-port $MATTER_PORT")
 
+if [ "$SSL" = "true" ]; then
+  MATTERBRIDGE_OPTIONS+=("-ssl")
+fi
+
 matterbridge "${MATTERBRIDGE_OPTIONS[@]}"
